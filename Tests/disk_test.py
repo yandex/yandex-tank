608bb13d981023c6862286e1db25b49f2415fccd
Andrey Pohilko <apc@apc.kg>
2012-09-27 16:35:09 +0400

    Fix unit test

--- Tests/disk_test.py
+++ Tests/disk_test.py
@@ -13,9 +13,9 @@ class  Disk_TestCase(unittest.TestCase):
         print self.foo.check()
         self.assertEquals(2, len(self.foo.check()))
         self.assertNotEquals(['', ''], self.foo.check())
-        fd = tempfile.mkstemp()
+        fd = tempfile.mkstemp()[0]
         os.write(fd, ' ' * 5000)
-        time.sleep(5)
+        time.sleep(1)
         self.assertNotEquals(['0', '0'], self.foo.check())
 
     def test_cols(self):
