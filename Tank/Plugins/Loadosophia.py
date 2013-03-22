46be088f739de5d21572a1a7d39f103c5fa8a444
Andrey Pohilko <apc@apc.kg>
2013-03-22 16:44:36 +0400

    Fix bug with disabling online

--- Tank/Plugins/Loadosophia.py
+++ Tank/Plugins/Loadosophia.py
@@ -47,7 +47,7 @@ class LoadosophiaPlugin(AbstractPlugin, AggregateResultListener):
         self.project_key = self.get_option("project", 'DEFAULT')
         self.title = self.get_option("test_title", "")
         self.color = self.get_option("color_flag", "")
-        self.online_enabled = self.get_option("online_enabled", "0")
+        self.online_enabled = int(self.get_option("online_enabled", "0"))
 
         try:
             aggregator = self.core.get_plugin_of_type(AggregatorPlugin)
@@ -62,6 +62,7 @@ class LoadosophiaPlugin(AbstractPlugin, AggregateResultListener):
                 self.loadosophia.start_online(self.project_key, self.title)
             except Exception, exc:
                 self.log.warning("Problems starting online: %s", exc)
+                self.online_enabled=False
             
     
     def aggregate_second(self, second_aggregate_data):
