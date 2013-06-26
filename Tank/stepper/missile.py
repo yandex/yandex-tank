8071a2321645010127165f66c87d8ca96dd82567
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2013-06-26 13:28:34 +0400

    skip empty lines between missiles in ammo file

--- Tank/stepper/missile.py
+++ Tank/stepper/missile.py
@@ -6,6 +6,7 @@ from exceptions import AmmoFileError
 
 
 class HttpAmmo(object):
+
     def __init__(self, uri, headers, method='GET', http_ver='1.1'):
         self.method = method
         self.uri = uri
@@ -21,7 +22,9 @@ class HttpAmmo(object):
 
 
 class SimpleGenerator(object):
+
     '''Generates ammo based on given sample'''
+
     def __init__(self, missile_sample):
         self.missiles = cycle([(missile_sample.to_s(), None)])
 
@@ -35,12 +38,15 @@ class SimpleGenerator(object):
 
 
 class UriStyleGenerator(SimpleGenerator):
+
     '''Generates GET ammo based on given URI list'''
+
     def __init__(self, uris, headers, loop_limit=0, http_ver='1.1'):
         self.ammo_number = 0
         self.loop_limit = loop_limit
         self.uri_count = len(uris)
-        self.missiles = cycle([(HttpAmmo(uri, headers, http_ver=http_ver).to_s(), None) for uri in uris])
+        self.missiles = cycle(
+            [(HttpAmmo(uri, headers, http_ver=http_ver).to_s(), None) for uri in uris])
 
     def __iter__(self):
         for m in self.missiles:
@@ -55,7 +61,9 @@ class UriStyleGenerator(SimpleGenerator):
 
 
 class AmmoFileReader(SimpleGenerator):
+
     '''Read missiles from ammo file'''
+
     def __init__(self, filename, loop_limit=0):
         self.filename = filename
         self.loops = 0
@@ -65,18 +73,20 @@ class AmmoFileReader(SimpleGenerator):
         with open(self.filename, 'rb') as ammo_file:
             chunk_header = ammo_file.readline()
             while chunk_header:
-                try:
-                    fields = chunk_header.split()
-                    chunk_size = int(fields[0])
-                    marker = fields[1] if len(fields) > 1 else None
-                    missile = ammo_file.read(chunk_size)
-                    if len(missile) < chunk_size:
-                        raise AmmoFileError("Unexpected end of file: read %s bytes instead of %s" % (len(missile), chunk_size))
-                    yield (missile, marker)
-                except (IndexError, ValueError):
-                    raise AmmoFileError("Error while reading ammo file. Position: %s, header: '%s'" % (ammo_file.tell(), chunk_header))
+                if chunk_header.strip('\r\n') is not '':
+                    try:
+                        fields = chunk_header.split()
+                        chunk_size = int(fields[0])
+                        marker = fields[1] if len(fields) > 1 else None
+                        missile = ammo_file.read(chunk_size)
+                        if len(missile) < chunk_size:
+                            raise AmmoFileError(
+                                "Unexpected end of file: read %s bytes instead of %s" % (len(missile), chunk_size))
+                        yield (missile, marker)
+                    except (IndexError, ValueError):
+                        raise AmmoFileError(
+                            "Error while reading ammo file. Position: %s, header: '%s'" % (ammo_file.tell(), chunk_header))
+                    if not chunk_header and (self.loops < self.loop_limit or self.loop_limit == 0):
+                        self.loops += 1
+                        ammo_file.seek(0)
                 chunk_header = ammo_file.readline()
-                if not chunk_header and (self.loops < self.loop_limit or self.loop_limit == 0):
-                    self.loops += 1
-                    ammo_file.seek(0)
-                    chunk_header = ammo_file.readline()
