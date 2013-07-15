44ee9e768a25ffd1059b4009b41bd28dd3847e44
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2013-07-15 20:25:14 +0400

    flush ammo every 10000-th missile

--- Tank/stepper/stepper.py
+++ Tank/stepper/stepper.py
@@ -60,5 +60,9 @@ class Stepper(object):
         self.ammo = fmt.Stpd(progress(af, 'Ammo: '))
 
     def write(self, f):
+        missile_count = 0
         for missile in self.ammo:
+            missile_count += 1
             f.write(missile)
+            if missile_count % 10000:
+                f.flush()
