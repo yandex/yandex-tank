9abced412110c0ff79fb65aa750c2b6dc59563ff
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2013-08-15 19:19:08 +0400

    stpd file reader

--- Tank/stepper/format.py
+++ Tank/stepper/format.py
@@ -1,7 +1,8 @@
 '''
 Ammo formatters
 '''
-
+from module_exceptions import StpdFileError
+import logging
 
 class Stpd(object):
     '''
@@ -12,3 +13,39 @@ class Stpd(object):
 
     def __iter__(self):
         return ("%s %s %s\n%s\n" % (len(missile), timestamp, marker, missile) for timestamp, marker, missile in self.af)
+
+class StpdReader(object):
+
+    '''Read missiles from stpd file'''
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.log = logging.getLogger(__name__)
+        self.log.info("Loading stepped missiles from '%s'" % filename)
+
+    def __iter__(self):
+        def read_chunk_header(ammo_file):
+            chunk_header = ''
+            while chunk_header is '':
+                line = ammo_file.readline()
+                if line is '':
+                    return line
+                chunk_header = line.strip('\r\n')
+            return chunk_header
+        with open(self.filename, 'rb') as ammo_file:
+            chunk_header = read_chunk_header(ammo_file) #  if we got StopIteration here, the file is empty
+            while chunk_header is not '':
+                try:
+                    fields = chunk_header.split()
+                    chunk_size = int(fields[0])
+                    timestamp = int(fields[1])
+                    marker = fields[2] if len(fields) > 1 else None
+                    missile = ammo_file.read(chunk_size)
+                    if len(missile) < chunk_size:
+                        raise StpdFileError(
+                            "Unexpected end of file: read %s bytes instead of %s" % (len(missile), chunk_size))
+                    yield (timestamp, missile, marker)
+                except (IndexError, ValueError) as e:
+                    raise StpdFileError(
+                        "Error while reading ammo file. Position: %s, header: '%s', original exception: %s" % (ammo_file.tell(), chunk_header, e))
+                chunk_header = read_chunk_header(ammo_file)
