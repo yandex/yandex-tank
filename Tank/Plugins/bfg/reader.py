99c6b568adc2bf99a925422d9d1228de83ae1d58
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2013-08-26 20:31:28 +0400

    save expected rps info

--- Tank/Plugins/bfg/reader.py
+++ Tank/Plugins/bfg/reader.py
@@ -1,4 +1,5 @@
 from Tank.Plugins.Aggregator import AbstractReader
+from Tank.stepper import info as si
 
 
 class BFGReader(AbstractReader):
@@ -10,6 +11,7 @@ class BFGReader(AbstractReader):
     def __init__(self, aggregator, bfg):
         AbstractReader.__init__(self, aggregator)
         self.bfg = bfg
+        self.steps = map(list, si.status.get_info().steps)
 
     def get_next_sample(self, force):
         new_data = []
@@ -21,6 +23,21 @@ class BFGReader(AbstractReader):
                 self.data_buffer[cur_time] = []
             self.data_buffer[cur_time].append(list(sample))
         if self.data_queue and len(self.data_queue) > 1:
-            return self.pop_second()
+            res = self.pop_second()
+            res.overall.planned_requests = self.__get_expected_rps()
+            return res
         else:
             return None
+
+    def __get_expected_rps(self):
+        '''
+        Mark second with expected rps
+        '''
+        while self.steps and self.steps[0][1] < 1:
+            self.steps.pop(0)
+        
+        if not self.steps:
+            return 0
+        else:
+            self.steps[0][1] -= 1
+            return self.steps[0][0]
