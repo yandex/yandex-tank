a954bcf568710f04574c36bbd14b4dc79a628262
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2013-09-10 19:52:35 +0400

    result queue size limit

--- Tank/Plugins/bfg/worker.py
+++ Tank/Plugins/bfg/worker.py
@@ -25,7 +25,7 @@ class BFG(object):
         self.cached_stpd = cached_stpd
 
     def start(self):
-        results = mp.Queue()
+        results = mp.Queue(1024)
         self.worker = mp.Process(target=self._start, args=(results,))
         self.worker.start()
         self.results = results
