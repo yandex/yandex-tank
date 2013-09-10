83ac2b449bce39028fc6d8c0132457cb1edded9f
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2013-09-10 20:02:56 +0400

    remove size limit for results

--- Tank/Plugins/bfg/worker.py
+++ Tank/Plugins/bfg/worker.py
@@ -25,7 +25,7 @@ class BFG(object):
         self.cached_stpd = cached_stpd
 
     def start(self):
-        results = mp.Queue(8192)
+        results = mp.Queue()
         self.worker = mp.Process(target=self._start, args=(results,))
         self.worker.start()
         self.results = results
