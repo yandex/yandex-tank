c13778bef974d6912c1ecabb691f1bc7c631f73f
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2013-11-01 17:53:48 +0400

    smart uri marker

--- Tank/stepper/mark.py
+++ Tank/stepper/mark.py
@@ -1,7 +1,8 @@
 from uuid import uuid4
 
+
 __test_missile = """\
-POST /example/search HTTP/1.1\r
+POST /example/search/hello/help/us?param1=50&param2=0&param3=hello HTTP/1.1\r
 Connection: close\r
 Host: example.org\r
 Content-length: 32\r
@@ -9,23 +10,28 @@ Content-length: 32\r
 param1=50&param2=0&param3=hello
 """
 
-__test_missile = """\
-GET /example/search?param1=50&param2=0&param3=hello HTTP/1.1\r
-Connection: close\r
-Host: example.org\r
-Content-length: 32\r
-\r
-"""
-
 
 def __mark_by_uri(missile):
     return '_'.join(missile.split('\n', 1)[0].split(' ', 2)[1].split('?')[0].split('/'))
 
 
+class __UriMarker(object):
+    '''
+    Returns a uri marker function with requested limit
+
+    >>> marker = __UriMarker(2)
+    >>> marker(__test_missile)
+    '_example_search'
+    '''
+    def __init__(self, limit):
+        self.limit = limit
+
+    def __call__(self, missile):
+        return '_'.join(missile.split('\n', 1)[0].split(' ', 2)[1].split('?')[0].split('/')[0:self.limit+1])
+
 __markers = {
     'uniq': lambda m: uuid4().hex,
     'uri': __mark_by_uri,
-    '1': __mark_by_uri,
 }
 
 
@@ -40,19 +46,25 @@ def get_marker(marker_type):
     32
 
     >>> get_marker('uri')(__test_missile)
-    '_example_search'
+    '_example_search_hello_help_us'
 
     >>> marker = get_marker('non-existent')(__test_missile)
     Traceback (most recent call last):
       ...
     NotImplementedError: No such marker: "non-existent"
 
+    >>> get_marker('3')(__test_missile)
+    '_example_search_hello'
     '''
     if marker_type and marker_type is not '0':
-        if marker_type in __markers:
-            return __markers[marker_type]
-        else:
-            raise NotImplementedError(
-                'No such marker: "%s"' % marker_type)
+        try:
+            limit = int(marker_type)
+            return __UriMarker(limit)
+        except ValueError:
+            if marker_type in __markers:
+                return __markers[marker_type]
+            else:
+                raise NotImplementedError(
+                    'No such marker: "%s"' % marker_type)
     else:
         return lambda m: ''
