9d700ac7c280e3b82f0de3639471f2dec2db2221
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2014-02-20 17:12:08 +0400

    non-verbose tests by default

--- Tests/run_nosetest.sh
+++ Tests/run_nosetest.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nosetests --with-xunit --all-modules --traverse-namespace --with-coverage --cover-inclusive --cover-erase --cover-package=Tank -v
+nosetests --with-xunit --all-modules --traverse-namespace --with-coverage --cover-inclusive --cover-erase --cover-package=Tank $@
+
