5f017e72f58b6127f4620b66bb7d0179da83bd2b
Andrey Pohilko <apc@apc.kg>
2014-04-04 15:54:50 +0400

    experiment with helper scripts

--- bfg.sh
+++ bfg.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-yandex-tank -o "tank.plugin_phantom=" -o "tank.plugin_bfg=Tank/Plugins/BFG.py" "$@"
+$(dirname $0)/tank.py -o "tank.plugin_phantom=" -o "tank.plugin_bfg=Tank/Plugins/BFG.py" "$@"
