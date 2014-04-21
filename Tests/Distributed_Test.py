c6417d1d58dbbed11f7cb1a27c42273eb24f1d3c
Andrey Pohilko <apc@apc.kg>
2014-04-21 20:00:00 +0400

    Fix unit tests loop

--- Tests/Distributed_Test.py
+++ Tests/Distributed_Test.py
@@ -17,14 +17,30 @@ class DistributedPluginTestCase(TankTestCase):
         self.foo.api_client_class = FakeAPIClient
 
     def test_run_minimal(self):
+
         self.core = self.get_core()
         self.core.set_option(DistributedPlugin.SECTION, "tanks_pool", "localhost")
         self.foo = DistributedPlugin(self.core)
         self.foo.api_client_class = FakeAPIClient
         self.foo.configure()
+
+        for mock in self.foo.api_clients:
+            mock.get_data.append({"ticket": str(time.time())})
+            mock.post_data.append({})
+            mock.get_data.append({"status": TankAPIClient.STATUS_PREPARED, "exclusive": 1})
+
+            mock.get_data.append({})
+            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHED, "exclusive": 1, "exitcode": 0})
+            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHED, "exclusive": 1, "exitcode": 0})
+            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHED, "exclusive": 1, "exitcode": 0})
+            mock.get_data.append(
+                {"status": TankAPIClient.STATUS_FINISHED, "exitcode": 0,
+                 "artifacts": ["tank.log", "test.log", "phout.txt"]})
+
         self.foo.prepare_test()
         self.foo.start_test()
         self.assertEquals(-1, self.foo.is_test_finished())
+        time.sleep(self.foo.retry_interval + 1)
         self.assertEquals(0, self.foo.is_test_finished())
         self.foo.end_test(0)
         self.foo.post_process(0)
@@ -42,16 +58,20 @@ class DistributedPluginTestCase(TankTestCase):
 
             mock.get_data.append({})
             mock.get_data.append({"status": TankAPIClient.STATUS_RUNNING, "exclusive": 1})
-            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHING, "exclusive": 1})
-            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHING, "exclusive": 1})
-            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHED, "exclusive": 1})
-            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHED, "artifacts": ["tank.log", "test.log", "phout.txt"]})
+            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHING, "exclusive": 1, "exitcode": 0, })
+            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHING, "exclusive": 1, "exitcode": 0, })
+            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHED, "exclusive": 1, "exitcode": 0, })
+            mock.get_data.append({"status": TankAPIClient.STATUS_FINISHED, "exclusive": 1, "exitcode": 0, })
+            mock.get_data.append(
+                {"status": TankAPIClient.STATUS_FINISHED, "exitcode": 0,
+                 "artifacts": ["tank.log", "test.log", "phout.txt"]})
             mock.get_data.append("some content")
             mock.get_data.append("some more content")
 
         self.foo.prepare_test()
         self.foo.start_test()
         self.assertEquals(-1, self.foo.is_test_finished())
+        time.sleep(self.foo.retry_interval + 1)
         self.assertEquals(0, self.foo.is_test_finished())
         self.foo.end_test(0)
         self.foo.post_process(0)
@@ -87,6 +107,7 @@ class FakeAPIClient(TankAPIClient):
         logging.debug("Saving data to %s", local_name)
         with open(local_name, "wb") as fd:
             fd.write("%s" % resp)
+
 
 if __name__ == '__main__':
     unittest.main()
