cf6d0537c7739cbfacfb6ad9aed7e3f22c81714d
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2014-05-29 23:08:04 +0400

    simple HTTP gun in BFG based on python-requests

--- Tank/Plugins/bfg/guns.py
+++ Tank/Plugins/bfg/guns.py
@@ -9,6 +9,11 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy import exc
 
+import requests
+
+requests_logger = logging.getLogger('requests')
+requests_logger.setLevel(logging.WARNING)
+
 Sample = namedtuple(
     'Sample', 'marker,threads,overallRT,httpCode,netCode,sent,received,connect,send,latency,receive,accuracy')
 
@@ -102,3 +107,34 @@ class CustomGun(AbstractPlugin):
 
     def shoot(self, missile, marker):
         return self.module.shoot(self, missile, marker)
+
+class HttpGun(AbstractPlugin):
+    SECTION = 'http_gun'
+
+    def __init__(self, core):
+        self.log = logging.getLogger(__name__)
+        AbstractPlugin.__init__(self, core)
+        self.base_address = self.get_option("base_address")
+
+    def shoot(self, missile, marker):
+        self.log.debug("Missile: %s\n%s", marker, missile)
+        start_time = time.time()
+        r = requests.get(self.base_address + missile)
+        errno = 0
+        httpCode = r.status_code
+        rt = int((time.time() - start_time) * 1000)
+        data_item = Sample(
+            marker,             # marker
+            th.active_count(),  # threads
+            rt,                 # overallRT
+            httpCode,           # httpCode
+            errno,              # netCode
+            0,                  # sent
+            0,                  # received
+            0,                  # connect
+            0,                  # send
+            rt,                 # latency
+            0,                  # receive
+            0,                  # accuracy
+        )
+        return (int(time.time()), data_item)
