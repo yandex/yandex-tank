872940a226a42e5e4e18f1a16aebc20b64364a30
Alexey Lavrenuke <direvius@213.180.218.144-red.dhcp.yndx.net>
2014-09-10 16:17:28 +0400

    Adapted for PUSH/PULL and JSON

--- Tank/Plugins/bfg/zmq_reader.py
+++ Tank/Plugins/bfg/zmq_reader.py
@@ -1,6 +1,6 @@
 import zmq
 import logging
-import pickle
+import json
 
 class ZmqReader(object):
 
@@ -10,15 +10,14 @@ class ZmqReader(object):
         self.queue = "tcp://127.0.0.1:43000" #queue
         self.log = logging.getLogger(__name__)
         context = zmq.Context()
-        self.socket = context.socket(zmq.REP)
-        self.socket.bind(self.queue)
-        self.log.info("ZMQ: Waiting for missiles to '%s'" % self.queue)
+        self.socket = context.socket(zmq.PULL)
+        self.socket.connect(self.queue)
+        self.log.info("ZMQ: Waiting for missiles from '%s'" % self.queue)
 
     def __iter__(self):
         while True:
           try:
               data = self.socket.recv()
-              self.socket.send("OK")
-              yield(pickle.loads(data))
+              yield(tuple(json.loads(data)))
           except Exception as e:
               print(e)
