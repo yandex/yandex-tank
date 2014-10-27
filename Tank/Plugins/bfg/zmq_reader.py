452afdfff9db8b607a113ccee15a0cb9aa711a20
Anton Kortunov <toshik@yandex-team.ru>
2014-10-27 14:25:55 +0400

    ZMQ rework in BFG plugin
    Stop iteration when 'stop' ammo received
    Stop ZMQ's eventloop when tank stops

--- Tank/Plugins/bfg/zmq_reader.py
+++ Tank/Plugins/bfg/zmq_reader.py
@@ -1,13 +1,15 @@
 import zmq
 import logging
 import json
+import threading as th
 
 class ZmqReader(object):
 
     '''Read missiles from zmq'''
 
-    def __init__(self):
-        self.queue = "tcp://127.0.0.1:43000" #queue
+    def __init__(self, queue):
+        self.queue = queue
+        self.quit = th.Event()
         self.log = logging.getLogger(__name__)
         context = zmq.Context()
         self.socket = context.socket(zmq.PULL)
@@ -17,7 +19,23 @@ class ZmqReader(object):
     def __iter__(self):
         while True:
           try:
-              data = self.socket.recv()
-              yield(tuple(json.loads(data)))
+              if not self.socket.poll(500):
+                  if self.quit.is_set():
+                      raise StopIteration()
+                  continue
+
+              data = tuple(self.socket.recv_json())
+
+              if len(data) != 3:
+                  if data[0] == "stop":
+                      raise StopIteration()
+                  continue
+
+              yield(data)
+          except StopIteration:
+              raise
           except Exception as e:
               print(e)
+
+    def stop(self):
+        self.quit.set()
