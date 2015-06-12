37dc212fcc20487fc5603d80eaf041cb404163ab
Alexey Lavrenuke <direvius@gmail.com>
2015-06-12 08:25:37 +0300

    rework it in the morning 8)

--- yandextank/plugins/bfg/worker.py
+++ yandextank/plugins/bfg/worker.py
@@ -6,12 +6,19 @@ import multiprocessing as mp
 import threading as th
 from Queue import Empty, Full
 
+
 def signal_handler(signum, frame):
     pass
 
 
 class BFG(object):
-    def __init__(self, gun, instances, threads, stpd_filename, zmq=False, cached_stpd=False):
+    """
+    A BFG load generator that manages multiple workers as processes and
+    threads in each of them and feeds them with tasks
+    """
+    def __init__(
+            self, gun, instances, threads,
+            stpd_filename, zmq=False, cached_stpd=False):
         self.log = logging.getLogger(__name__)
         self.log.info(
             """
@@ -28,7 +35,7 @@ Gun: {gun.__class__}
         self.gun = gun
         self.instances = int(instances)
         self.threads = int(threads)
-        self.results = mp.Queue(16384)
+        self.results = mp.Queue()
         self.quit = mp.Event()
         self.task_queue = mp.Queue(1024)
         self.cached_stpd = cached_stpd
@@ -47,13 +54,24 @@ Gun: {gun.__class__}
         self.feeder.start()
 
     def running(self):
+        """
+        True while there are alive workers out there. Tank
+        will quit when this would become False
+        """
         return not self.workers_finished
 
     def stop(self):
+        """
+        Say the workers to finish their jobs and quit.
+        """
         self.quit.set()
-        self.plan.stop()
+        if self.zmq:
+            self.plan.stop()
 
     def _feed(self):
+        """
+        A feeder that runs in distinct thread in main process.
+        """
         if self.zmq:
             print "Starting ZMQ: connecting to ", self.zmq
             self.plan = ZmqReader(self.zmq)
@@ -64,8 +82,9 @@ Gun: {gun.__class__}
         for task in self.plan:
             if self.quit.is_set():
                 self.log.info("Stop feeding: gonna quit")
-                # self.task_queue.close()
                 return
+            # try putting a task to a queue unless there is a quit flag
+            # or all workers have exited
             while 1:
                 try:
                     self.task_queue.put(task, timeout=1)
@@ -75,11 +94,12 @@ Gun: {gun.__class__}
                         return
                     else:
                         continue
+        workers_count = self.threads * self.instances
         self.log.info(
             "Feeded all data. Publishing %d killer tasks" % (
-                self.threads * self.instances))
+                workers_count))
         [self.task_queue.put(None, timeout=1) for _ in xrange(
-            0, self.threads * self.instances)]
+            0, workers_count)]
 
         try:
             self.log.info("Waiting for workers")
@@ -87,14 +107,18 @@ Gun: {gun.__class__}
             self.log.info("All workers exited.")
             self.workers_finished = True
         except (KeyboardInterrupt, SystemExit):
+            self.task_queue.close()
+            self.results.close()
             self.quit.set()
-            # self.task_queue.close()
-            self.log.info("Waiting for workers")
+            self.log.info("Going to quit. Waiting for workers")
             map(lambda x: x.join(), self.pool)
             self.workers_finished = True
 
-
     def _worker(self):
+        """
+        A worker that runs in a distinct process and manages pool
+        of thread workers that do actual jobs
+        """
         self.log.info(
             "Started shooter process with %s threads..." % self.threads)
         pool = [th.Thread(
@@ -102,17 +126,22 @@ Gun: {gun.__class__}
         map(lambda x: x.start(), pool)
         try:
             map(lambda x: x.join(), pool)
-            self.log.info("Exiting shooter process...")
+            self.log.debug("Exiting shooter process.")
         except (KeyboardInterrupt, SystemExit):
             self.quit.set()
+            map(lambda x: x.join(), pool)  # wait for workers to finish
 
     def _thread_worker(self):
-        self.log.info("Starting shooter thread %s", th.current_thread().name)
+        """
+        A worker that does actual jobs
+        """
+        self.log.debug("Starting shooter thread %s", th.current_thread().name)
         while not self.quit.is_set():
             try:
                 task = self.task_queue.get(timeout=1)
                 if not task:
-                    self.log.info("%s got killer task.", th.current_thread().name)
+                    self.log.info(
+                        "%s got killer task.", th.current_thread().name)
                     break
                 timestamp, missile, marker = task
                 planned_time = self.start_time + (timestamp / 1000.0)
@@ -121,12 +150,14 @@ Gun: {gun.__class__}
                     time.sleep(delay)
                 self.gun.shoot(missile, marker, self.results)
             except (KeyboardInterrupt, SystemExit):
-                self.quit.set()
+                break
             except Empty:
                 if self.quit.is_set():
-                    self.log.info("Empty queue. Exiting thread %s", th.current_thread().name)
+                    self.log.debug(
+                        "Empty queue. Exiting thread %s",
+                        th.current_thread().name)
                     return
             except Full:
                 self.log.warning(
                     "Couldn't put to result queue because it's full")
-        self.log.info("Exiting shooter thread %s", th.current_thread().name)
+        self.log.debug("Exiting shooter thread %s", th.current_thread().name)
