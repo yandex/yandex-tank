f2834a24af88372ec279ce2a22122a3ff1f0f645
Timur Torubarov <netort@yandex-team.ru>
2016-01-15 14:47:56 +0300

    cosmetics

--- yandextank/api/apiworker-usage-sample.py
+++ yandextank/api/apiworker-usage-sample.py
@@ -8,14 +8,13 @@ logger.setLevel(logging.DEBUG)
 
 #not mandatory options below:
 options = dict()
-options['config'] = '/home/netort/load.ini'
+options['config'] = '/path/to/config/load.ini'
 options['manual_start'] = "1"
 options['user_options'] = [
-    'phantom.ammofile=/home/netort/ammofile',
-    'meta.notify=netort',
+    'phantom.ammofile=/path/to/ammofile',
     'phantom.rps_schedule=const(1,2m)',
 ]
-log_filename = '/home/netort/tank.log'
+log_filename = '/path/to/log/tank.log'
 #======================================
 
 apiworker = ApiWorker()
@@ -24,4 +23,4 @@ try:
     apiworker.configure(options)
     apiworker.perform_test()
 except Exception, ex:
-    logger.error('Error tring to perform a test: %s', ex)
+    logger.error('Error trying to perform a test: %s', ex)
