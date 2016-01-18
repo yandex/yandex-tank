fca5794801f3b8b0cd9d568c74bd79c717f0c16b
Alexey Lavrenuke <direvius@gmail.com>
2016-01-18 16:21:54 +0300

    delete obsolete scripts

--- scripts/bfg.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-yandex-tank -o "tank.plugin_phantom=" -o "tank.plugin_bfg=Tank/Plugins/BFG.py" "$@"
--- scripts/jmeter.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-yandex-tank -o "tank.plugin_phantom=" -o "tank.plugin_jmeter=Tank/Plugins/JMeter.py" "$@"
