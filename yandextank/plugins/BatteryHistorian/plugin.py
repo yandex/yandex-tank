3440efc47cb68a14a6fe8713dc965be0239f729d
Timur Torubarov <netort@yandex-team.ru>
2016-06-14 16:36:21 +0300

    bugfixes, cosmetics

--- yandextank/plugins/BatteryHistorian/plugin.py
+++ yandextank/plugins/BatteryHistorian/plugin.py
@@ -23,10 +23,10 @@ class Plugin(AbstractPlugin):
         self.default_target = None
         self.device_id = None
         self.cmds = {
-            "enable_full_log" : shlex.split("adb shell dumpsys batterystats --enable full-wake-history"),
-            "disable_full_log" : shlex.split("adb shell dumpsys batterystats --disable full-wake-history"),
-            "reset" : shlex.split("adb shell dumpsys batterystats --reset"),
-            "dump": shlex.split("adb shell dumpsys batterystats")
+            "enable_full_log" : "adb %s shell dumpsys batterystats --enable full-wake-history",
+            "disable_full_log" : "adb %s shell dumpsys batterystats --disable full-wake-history",
+            "reset" : "adb %s shell dumpsys batterystats --reset",
+            "dump": "adb %s shell dumpsys batterystats"
         }
 
     def get_available_options(self):
@@ -34,25 +34,24 @@ class Plugin(AbstractPlugin):
 
     def configure(self):
         self.device_id = self.get_option("device_id", None).strip()
-        if self.device_id:
-            for key, value in self.cmds.iteritems():
-                self.cmds['key'] = "{origin} -s {device_id}".format(
-                    origin=value,
-                    device_id=self.device_id
-                )
-            logger.debug('cmds with device_ids: %s', self.cmds)
+        if not self.device_id:
+            modify = ''
+        else:
+            modify = '-s {device_id}'.format(device_id=self.device_id)
+        for key, value in self.cmds.iteritems():
+            self.cmds[key] = value % modify
 
         self.logfile = self.core.mkstemp(".log", "battery_historian_")
         self.core.add_artifact_file(self.logfile)
 
     def prepare_test(self):
-        subprocess.call(self.cmds['enable_full_log'], shell=True)
-        subprocess.call(self.cmds['reset'], shell=True)
+        out = subprocess.check_output(self.cmds['enable_full_log'], shell=True)
+        out = subprocess.check_output(self.cmds['reset'], shell=True)
 
     def end_test(self, retcode):
-        out = subprocess.Popen(self.cmds['dump'], stdout=subprocess.PIPE).communicate()[0]
-        subprocess.call(self.cmds['disable_full_log'], shell=True)
-        subprocess.call(self.cmds['reset'], shell=True)
+        out = subprocess.Popen(self.cmds['dump'], stdout=subprocess.PIPE, shell=True).communicate()[0]
+        subprocess.check_output(self.cmds['disable_full_log'], shell=True)
+        subprocess.check_output(self.cmds['reset'], shell=True)
         with open(self.logfile, 'w') as f:
             f.write(out)
         return retcode
