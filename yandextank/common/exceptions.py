c1b6387d30e855be7ad3535c5ad696f7c5105643
Arseniy Fomchenko <fomars@yandex-team.ru>
2016-10-17 13:47:01 +0300

    writing phantom info to core job

--- yandextank/common/exceptions.py
+++ yandextank/common/exceptions.py
@@ -3,3 +3,11 @@ class PluginImplementationError(RuntimeError):
     Error in plugin implementation
     """
     pass
+
+
+class PluginNotPrepared(Exception):
+    """
+    Can't find plugin's info in core.job
+    """
+    def __init__(self, msg):
+        self.message = "%s\n%s" % (self.__doc__, msg)
