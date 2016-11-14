7f04d9519321d2dedccaf9de53b2148bf345ec71
Arseniy Fomchenko <fomars@yandex-team.ru>
2016-11-14 19:01:05 +0300

    JsonReport proper monitoring logging

--- yandextank/plugins/JsonReport/plugin.py
+++ yandextank/plugins/JsonReport/plugin.py
@@ -1,7 +1,7 @@
 # TODO: make the next two lines unnecessary
 # pylint: disable=line-too-long
 # pylint: disable=missing-docstring
-
+import json
 import logging
 import os
 
@@ -16,6 +16,10 @@ logger = logging.getLogger(__name__)  # pylint: disable=C0103
 class Plugin(AbstractPlugin, AggregateResultListener, MonitoringDataListener):
     # pylint:disable=R0902
     SECTION = 'json_report'
+
+    def __init__(self, core):
+        super(Plugin, self).__init__(core)
+        self._is_telegraf = None
 
     def get_available_options(self):
         return ['monitoring_log', 'test_data_log', 'test_stats_log']
@@ -52,4 +56,15 @@ class Plugin(AbstractPlugin, AggregateResultListener, MonitoringDataListener):
         self.stats_logger.info(stats)
 
     def monitoring_data(self, data_list):
+        if self.is_telegraf:
+            self.monitoring_logger.info(json.dumps(data_list))
+        else:
+            [self.monitoring_logger.info(data) for data in data_list]
+
         self.monitoring_logger.info(data_list)
+
+    @property
+    def is_telegraf(self):
+        if self._is_telegraf is None:
+            self._is_telegraph = 'Telegraf' in self.core.job.monitoring_plugin.__module__
+        return self._is_telegraph
