c4b55ba099a959f8bae40ff9aee3dbe3ffd670c3
Arseniy Fomchenko <fomars@yandex-team.ru>
2016-11-21 16:04:28 +0300

    removed tests that use external resourse

--- yandextank/common/tests/test_util.py
+++ yandextank/common/tests/test_util.py
@@ -4,32 +4,6 @@ import pytest
 from yandextank.common.util import AddressWizard, Drain
 
 
-class TestAddressWizard(object):
-
-    @pytest.mark.parametrize('address_str, do_test, explicit_port, expected', [
-        (u'[coverage-api01i.cloud.load.maps.yandex.net]:80',
-         False,
-         None,
-         (True, '2a02:6b8:c01:105::568:0:14', 80, 'coverage-api01i.cloud.load.maps.yandex.net')),
-        (u'[coverage-api01i.cloud.load.maps.yandex.net]:80',
-         True,
-         None,
-         (True, '2a02:6b8:c01:105::568:0:14', 80, 'coverage-api01i.cloud.load.maps.yandex.net')),
-        (u'[coverage-api01i.cloud.load.maps.yandex.net]:80',
-         False,
-         80,
-         (True, '2a02:6b8:c01:105::568:0:14', 80, 'coverage-api01i.cloud.load.maps.yandex.net')),
-        (u'[coverage-api01i.cloud.load.maps.yandex.net]:80',
-         True,
-         80,
-         (True, '2a02:6b8:c01:105:0:568:0:14', 80, 'coverage-api01i.cloud.load.maps.yandex.net'))
-    ])
-    def test_resolve(self, address_str, do_test, explicit_port, expected):
-        result = AddressWizard().resolve(address_str, do_test, explicit_port)
-        assert ipaddress.ip_address(unicode(result[1])) == ipaddress.ip_address(unicode(expected[1]))
-        assert result[2:4]
-
-
 class TestDrain(object):
     def test_run(self):
         """
