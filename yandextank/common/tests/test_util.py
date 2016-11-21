28a6e5d96b55e13a0b9b106d3700d87377357274
Arseniy Fomchenko <fomars@yandex-team.ru>
2016-11-21 14:16:56 +0300

    fix util tests

--- yandextank/common/tests/test_util.py
+++ yandextank/common/tests/test_util.py
@@ -1,5 +1,7 @@
+from Queue import Queue
+
 import pytest
-from yandextank.common.util import AddressWizard
+from yandextank.common.util import AddressWizard, Drain
 
 
 class TestAddressWizard(object):
@@ -24,3 +26,37 @@ class TestAddressWizard(object):
     ])
     def test_resolve(self, address_str, do_test, explicit_port, expected):
         assert AddressWizard().resolve(address_str, do_test, explicit_port) == expected
+
+
+class TestDrain(object):
+    def test_run(self):
+        """
+        Test drain's run function (in a same thread)
+        """
+        source = range(5)
+        destination = Queue()
+        drain = Drain(source, destination)
+        drain.run()
+        assert destination.qsize() == 5
+
+    def test_interrupt(self):
+        """
+        Test we can interrupt the drain
+        """
+        source = range(1000000)
+        destination = Queue()
+        drain = Drain(source, destination)
+        drain.start()
+        drain.close()
+        assert destination.qsize() < 1000000
+
+    def test_interrupt_and_wait(self):
+        """
+        Test we can interrupt the drain
+        """
+        source = range(1000000)
+        destination = Queue()
+        drain = Drain(source, destination)
+        drain.start()
+        drain.wait()
+        assert destination.qsize() == 1000000
