1aea496a9722d4535786fec9d58d8b4a1d2e8bec
Alexey Lavrenuke <direvius@gmail.com>
2017-03-24 17:05:08 +0300

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yandextank',
-    version='1.9.0',
+    version='1.8.33',
     description='a performance measurement tool',
     longer_description='''
 Yandex.Tank is a performance measurement and load testing automatization tool.
