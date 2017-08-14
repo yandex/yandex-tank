35c4bf5b275b0bef7651c52a857546fa60cd425b
fomars <fomars@yandex-team.ru>
2017-08-14 16:40:22 +0300

    docker + uwsgi

--- yandextank/master/server.py
+++ yandextank/master/server.py
@@ -1,5 +1,11 @@
 import connexion
 
-app = connexion.App(__name__, specification_dir='./')
-app.add_api('api.yaml')
-app.run(port=8080)
+
+def get_app():
+    app = connexion.App(__name__, specification_dir='./')
+    app.add_api('api.yaml')
+    return app.app
+    # app.run(port=80)
+
+
+application = get_app()
