c509861fe6d052b8bdcbf94a05211de16cee833b
Stanislav Ievlev <stanislav.ievlev@gmail.com>
2017-09-07 15:39:15 +0300

    Add basic class for all 'tail -f' like stats readers

--- yandextank/common/tests/test_util.py
+++ yandextank/common/tests/test_util.py
@@ -1,6 +1,5 @@
 from queue import Queue
-from yandextank.common.util import Drain, Chopper
-
+from yandextank.common.util import Drain, Chopper, FileScanner
 
 class TestDrain(object):
     def test_run(self):
@@ -41,3 +40,34 @@ class TestChopper(object):
         source = (range(i) for i in range(5))
         expected = [0, 0, 1, 0, 1, 2, 0, 1, 2, 3]
         assert list(Chopper(source)) == expected
+
+
+class TestFileScanner(object):
+    @staticmethod
+    def __process_chunks(chunks, sep="\n"):
+        reader = FileScanner("somefile.txt", sep=sep)
+        result = []
+        for chunk in chunks:
+            result.extend(reader._read_lines(chunk))
+        return result
+
+    def test_empty(self):
+        assert self.__process_chunks([""]) == []
+
+    def test_simple(self):
+        assert self.__process_chunks(["aaa\n", "bbb\n", "ccc\n"]) == ["aaa", "bbb", "ccc"]
+
+    def test_split(self):
+        assert self.__process_chunks(["aaa\nbbb\n", "ccc\n"]) == ["aaa", "bbb", "ccc"]
+
+    def test_join(self):
+        assert self.__process_chunks(["aaa", "bbb\n", "ccc\n"]) == ["aaabbb", "ccc"]
+
+    def test_no_first_separator(self):
+        assert self.__process_chunks(["aaa"]) == []
+
+    def test_no_last_separator(self):
+        assert self.__process_chunks(["aaa\n", "bbb\n", "ccc"]) == ["aaa", "bbb"]
+
+    def test_use_custom_separator(self):
+        assert self.__process_chunks(["aaa:bbb:ccc:"], ":") == ["aaa", "bbb", "ccc"]
