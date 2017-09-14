8de21dffef61fd78e888e400560d36c9806fe0c6
fomars <fomars@yandex-team.ru>
2017-09-14 17:26:14 +0300

    fixed & tested

--- yandextank/plugins/DataUploader/cli.py
+++ yandextank/plugins/DataUploader/cli.py
@@ -15,7 +15,10 @@ from urlparse import urljoin
 from datetime import datetime
 import pkg_resources
 import yaml
+from cerberus import Validator
 
+from yandextank.core import TankCore
+from yandextank.validator.validator import ValidationError, load_yaml_schema
 from .client import APIClient, OverloadClient
 from .plugin import LPJob, BackendTypes
 from .plugin import Plugin as DataUploader
@@ -53,18 +56,6 @@ def from_tank_config(test_dir):
     return config
 
 
-def get_lp_config(config):
-    """
-    looks for config file in shooting_dir,
-    returns config dict of section 'meta'
-    :rtype: dict
-    """
-    lp_config = dict(config.items(SECTION))
-    for key in sorted(lp_config.keys()):
-        logger.debug('%s: %s' % (key, lp_config[key]))
-    return lp_config
-
-
 def check_log(log_name):
     assert os.path.exists(log_name), \
         'Data log {} not found\n'.format(log_name) + \
@@ -89,18 +80,15 @@ def upload_monitoring(shooting_dir, log_name, lp_job):
     check_log(data_log)
     sys.stdout.write('Uploading monitoring data')
     with open(data_log, 'r') as f:
-        for line in f:
-            lp_job.push_monitoring_data(line.strip())
+        for line in f.readlines():
+            lp_job.push_monitoring_data(json.loads(line.strip()))
             sys.stdout.write('.')
             sys.stdout.flush()
     sys.stdout.write('\n')
 
 
 def send_config_snapshot(config, lp_job):
-    config.set(SECTION, 'launched_from', 'post-loader')
-    output = StringIO()
-    config.write(output)
-    lp_job.send_config_snapshot(output.getvalue())
+    lp_job.send_config_snapshot(yaml.dump(config))
 
 
 def edit_metainfo(lp_config, lp_job):
@@ -130,10 +118,17 @@ def make_symlink(shooting_dir, name):
     logger.info('Symlink created: {}'.format(os.path.abspath(link_name)))
 
 
-def post_loader():
-    parser = argparse.ArgumentParser()
+class ConfigError(Exception):
+    pass
 
-    parser.add_argument('-c', '--config', help='YAML config')
+
+def post_loader():
+    CONFIG_SCHEMA = load_yaml_schema(pkg_resources.resource_filename('yandextank.plugins.DataUploader',
+                                                                     'config/postloader_schema.yaml'))
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('-c', '--config', help='YAML config. Format:\n{}'.format(yaml.dump(CONFIG_SCHEMA)))
     parser.add_argument('-a', '--api_address',
                         help='service to upload test results to, e.g. https://overload.yandex.net')
     parser.add_argument('-t', '--target', help='Address of the tested target, host[:port]')
@@ -148,14 +143,18 @@ def post_loader():
     assert os.path.exists(args.test_dir), 'Directory {} not found'.format(args.test_dir)
     # load cfg
     if args.config:
-        config = yaml.load(args.config)
+        with open(args.config) as f:
+            config = yaml.load(f)
     else:
         config = from_tank_config(args.test_dir)
     # parse target host and port
-    try:
-        target_host, target_port = args.target.rsplit(':', 1)
-    except ValueError:
-        target_host, target_port = args.target, None
+    if args.target is not None:
+        try:
+            target_host, target_port = args.target.rsplit(':', 1)
+        except ValueError:
+            target_host, target_port = args.target, None
+    else:
+        target_host, target_port = None, None
     # update cfg from cli options
     for key, value in [('api_address', args.api_address),
                        ('target_host', target_host),
@@ -167,6 +166,13 @@ def post_loader():
                        ('token_file', args.token)]:
         if value is not None:
             config[key] = value
+    # Validation
+    v = Validator(schema=CONFIG_SCHEMA,
+                  allow_unknown=True)
+    if not v.validate(config):
+        raise ValidationError(v.errors)
+    config = v.normalized(config)
+
     # lunapark or overload?
     backend_type = BackendTypes.identify_backend(config['api_address'])
     if backend_type == BackendTypes.LUNAPARK:
@@ -174,12 +180,17 @@ def post_loader():
         api_token = None
     elif backend_type == BackendTypes.OVERLOAD:
         client = OverloadClient
-        api_token = DataUploader.read_token(config["token_file"])
+        try:
+            api_token = DataUploader.read_token(config["token_file"])
+        except KeyError:
+            raise ConfigError('Token file required')
     else:
         raise RuntimeError("Backend type doesn't match any of the expected")
 
+    user_agent = ' '.join(('Uploader/{}'.format(DataUploader.VERSION),
+                           TankCore.get_user_agent()))
     api_client = client(base_url=config['api_address'],
-                        user_agent='Lunapark/{}'.format(pkg_resources.require('yatank-internal-lunapark')[0].version),
+                        user_agent=user_agent,
                         api_token=api_token
                         # todo: add timeouts
                         )
@@ -187,11 +198,8 @@ def post_loader():
         client=api_client,
         target_host=config.get('target_host'),
         target_port=config.get('target_port'),
-        person=config.get(
-            'operator',
-            '') or pwd.getpwuid(
-            os.geteuid())[0],
-        task=config['task'],
+        person=config.get('operator') or pwd.getpwuid(os.geteuid())[0],
+        task=config.get('task'),
         name=config['job_name'],
         description=config['job_dsc'],
         tank=socket.getfqdn())
