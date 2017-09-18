437ffcb2c01aa5320e35c29bbaa922bc99a71fc4
fomars <fomars@yandex-team.ru>
2017-09-18 15:29:57 +0300

    requests ids - reformatted

--- yandextank/plugins/DataUploader/client.py
+++ yandextank/plugins/DataUploader/client.py
@@ -3,7 +3,6 @@ import time
 import traceback
 import urllib
 
-import yaml
 from future.moves.urllib.parse import urljoin
 from builtins import range
 
@@ -123,23 +122,39 @@ class APIClient(object):
         p = self.session.prepare_request(req)
         request_id = self.ids.next()
         if trace:
-            logger.debug("Making request:\n{}".format(yaml.dump({
-                'id': request_id,
-                'method': p.method,
-                'url': p.url,
-                'headers': p.headers,
-                'body': p.body
-            })))
+            logger.debug("""
+            Request:
+            id: {}
+            method: {}
+            url: {}
+            headers: {}
+            body: {}""".format(request_id, p.method, p.url, p.headers, p.body))
+            # logger.debug("Making request:\n{}".format(yaml.dump({
+            #     'id': request_id,
+            #     'method': p.method,
+            #     'url': p.url,
+            #     'headers': p.headers,
+            #     'body': p.body
+            # })))
         resp = self.session.send(p, timeout=self.connection_timeout)
         if trace:
-            logger.debug("Got response in {}s:\n{}".format(resp.elapsed.total_seconds(),
-                                                           yaml.dump({
-                                                               'id': request_id,
-                                                               'reason': resp.reason,
-                                                               'status code': resp.status_code,
-                                                               'headers': self.filter_headers(resp.headers),
-                                                               'content': resp.content
-                                                           })))
+            logger.debug("""
+            Response:
+            id: {}
+            elapsed time: {}
+            reason: {}
+            status code: {}
+            headers: {}
+            content: {}""".format(request_id, resp.elapsed.total_seconds(),
+                                  resp.reason, resp.status_code, self.filter_headers(resp.headers), resp.content))
+            # logger.debug("Got response in {}s:\n{}".format(resp.elapsed.total_seconds(),
+            #                                                yaml.dump({
+            #                                                    'id': request_id,
+            #                                                    'reason': resp.reason,
+            #                                                    'status code': resp.status_code,
+            #                                                    'headers': self.filter_headers(resp.headers),
+            #                                                    'content': resp.content
+            #                                                })))
         if resp.status_code in [500, 502, 503, 504]:
             raise self.NotAvailable(
                 request="request: %s %s\n\tHeaders: %s\n\tBody: %s" %
