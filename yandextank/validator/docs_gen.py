2f02b7e147de719d35c910dcf4da6cf975c42a33
fomars <fomars@yandex-team.ru>
2017-09-24 02:50:25 +0300

    bullet list, tests

--- yandextank/validator/docs_gen.py
+++ yandextank/validator/docs_gen.py
@@ -29,9 +29,8 @@ class TextBlock(object):
 
 
 class RSTFormatter(object):
-
     @staticmethod
-    def any_of(blocks):
+    def any_of_table(blocks):
         """
 
         :type blocks: list of TextBlock
@@ -45,12 +44,13 @@ class RSTFormatter(object):
         width = max((len(HEADER), sum([c.padded_width for c in blocks]))) + (cnt + 1)
         height = max([c.height for c in blocks])
         # rows separators
-        top_bar = '+{}+'.format('-' * (width-2))
+        top_bar = '+{}+'.format('-' * (width - 2))
         header_bar = '+{}+'.format('+'.join(['=' * c.padded_width for c in blocks]))
         bottom_bar = '+{}+'.format('+'.join(['-' * c.padded_width for c in blocks]))
 
-        header = '|{}|'.format(HEADER.center(width-2))
-        body = '\n'.join(['| {} |'.format(' | '.join([c.get_line_justified(i) for c in blocks])) for i in range(height)])
+        header = '|{}|'.format(HEADER.center(width - 2))
+        body = '\n'.join(
+            ['| {} |'.format(' | '.join([c.get_line_justified(i) for c in blocks])) for i in range(height)])
         return '\n'.join([top_bar,
                           header,
                           header_bar,
@@ -65,11 +65,73 @@ class RSTFormatter(object):
         """
         return '\n'.join(['| {}'.format(line) for line in block.lines])
 
+    @staticmethod
+    def title(content, new_line_replacement=' ', tab_replacement='  '):
+        prepared_content = content.strip().replace('\n', new_line_replacement).replace('\t', tab_replacement)
+        return u'{}\n{}'.format(prepared_content, '=' * len(prepared_content))
 
-def make_doc(schema, formatter):
-    paragraphs = [formatter.paragraph(key, value) for key,value in schema.items()]
+    @staticmethod
+    def subtitle(content, new_line_replacement=' ', tab_replacement='  '):
+        prepared_content = content.strip().replace('\n', new_line_replacement).replace('\t', tab_replacement)
+        return u'{}\n{}'.format(prepared_content, '-' * len(prepared_content))
 
-    pass
+    @staticmethod
+    def emphasis(block):
+        """
+
+        :type block: TextBlock
+        """
+        return '\n'.join(['*{}*'.format(line) for line in block.lines])
+
+    @classmethod
+    def bullet_list(cls, blocks):
+        return '\n'.join([cls._list_item(block) for block in blocks])
+
+    @staticmethod
+    def _list_item(block):
+        """
+
+        :type block: TextBlock
+        """
+        return '- ' + '\n  '.join(block.lines)
+
+
+def format_schema(schema, formatter):
+    """
+
+    :param schema: Mapping
+    :type formatter: RSTFormatter
+    """
+    REQUIRED = 'required'
+    DEFAULT = 'default'
+
+    def default(_dict):
+        return '{}: {}'.format(DEFAULT, _dict.get(DEFAULT))
+
+    def is_required(_dict):
+        return _dict.get(REQUIRED, False)
+
+    STANDARD_CONVERTER = lambda _key, content: formatter.preserve_indents(
+        TextBlock(yaml.dump({_key: content}, default_flow_style=False)))
+
+    rules_converters = {
+        'anyof': lambda _key, content: formatter.any_of_table([TextBlock(yaml.dump(block, default_flow_style=False))
+                                                               for block in content])
+    }
+    options = {}
+
+    for key, value in schema.items():
+        # title = formatter.title(key)
+        subtitle = formatter.emphasis(TextBlock(REQUIRED)) if is_required(value) \
+            else formatter.emphasis(TextBlock(default(value)))
+        body = TextBlock(subtitle + '\n' +
+                         yaml.dump({k: v for k, v in value.items() if k not in (DEFAULT, REQUIRED)},
+                                   default_flow_style=False
+                                   ))
+        body = '\n'.join([rules_converters.get(k, STANDARD_CONVERTER)(v) for k, v in value])
+        options[key] = body
+
+    return '\n'.join(paragraphs)
 
 
 def main():
@@ -78,7 +140,7 @@ def main():
     schema_path = parser.parse_args().schema
     with open(schema_path) as f:
         schema = yaml.load(f)
-    document = make_doc(schema, RSTFormatter)
+    document = format_schema(schema, RSTFormatter())
 
 
 if __name__ == '__main__':
--- yandextank/validator/tests/test_docs_gen.py
+++ yandextank/validator/tests/test_docs_gen.py
@@ -1,25 +1,101 @@
+# coding=utf-8
 import pytest
-from yandextank.validator.docs_gen import RSTFormatter, TextBlock
 
-@pytest.mark.parametrize('s1, s2, expected', [
-    ('type: list\nelements:\n\ttype: string',
-     'type: string\nallowed: auto\ndescription:',
-     """+--------------------------------+
+from yandextank.validator.docs_gen import RSTFormatter, TextBlock, format_schema
+
+class TestRSTFormatter(object):
+
+    @pytest.mark.parametrize('s1, s2, expected', [
+        ('type: list\nelements:\n\ttype: string',
+         'type: string\nallowed: auto\ndescription:',
+         """+--------------------------------+
 |             any of             |
 +================+===============+
 | type: list     | type: string  |
 | elements:      | allowed: auto |
 |   type: string | description:  |
 +----------------+---------------+"""
-     )
-])
-def test_any_of(s1, s2, expected):
-    assert RSTFormatter.any_of([TextBlock(s1), TextBlock(s2)]) == expected
+         )
+    ])
+    def test_any_of_table(self, s1, s2, expected):
+        assert RSTFormatter.any_of_table([TextBlock(s1), TextBlock(s2)]) == expected
+
+    @pytest.mark.parametrize('s, expected',[
+        ('type: list\nelements:\n\ttype: string', '| type: list\n| elements:\n|   type: string'),
+        ('type: string\nallowed: auto\ndescription:', '| type: string\n| allowed: auto\n| description:')
+    ])
+    def test_preserve_indents(self, s, expected):
+        assert RSTFormatter.preserve_indents(TextBlock(s)) == expected
+
+    @pytest.mark.parametrize('s, expected',[
+        ('A nice title',
+         'A nice title\n============'),
+        ('A nice\ntitle',
+         'A nice title\n============'),
+        (u'Широкая электрификация южных губерний',
+         u'Широкая электрификация южных губерний\n====================================='),
+        (u'Широкая электрификация\nюжных губерний',
+         u'Широкая электрификация южных губерний\n====================================='),
+        (u'Широкая электрификация\n\tюжных губерний',
+         u'Широкая электрификация   южных губерний\n======================================='),
+    ])
+    def test_title(self, s, expected):
+        assert RSTFormatter.title(s) == expected
+
+    @pytest.mark.parametrize('block, expected', [
+        (':type: list\n:elements:\n\t:type: string',
+         """- :type: list
+  :elements:
+    :type: string"""
+         )
+    ])
+    def test__list_item(self, block, expected):
+        assert RSTFormatter._list_item(TextBlock(block)) == expected
+
+    @pytest.mark.parametrize('texts, expected', [
+        ([
+            ':type: list\n:elements:\n\t:type: string',
+            ':type: string\n:allowed: auto'
+        ],
+        """- :type: list
+  :elements:
+    :type: string
+- :type: string
+  :allowed: auto"""),
+        ([
+             ':type: list\n:elements:\n :type: string',
+             ':type: string\n:allowed: auto'
+         ],
+         """- :type: list
+  :elements:
+   :type: string
+- :type: string
+  :allowed: auto""")
+    ])
+    def test_any_of_list(self, texts, expected):
+        assert RSTFormatter.bullet_list([TextBlock(text) for text in texts]) == expected
 
 
-@pytest.mark.parametrize('s, expected',[
-    ('type: list\nelements:\n\ttype: string', '| type: list\n| elements:\n|   type: string'),
-    ('type: string\nallowed: auto\ndescription:', '| type: string\n| allowed: auto\n| description:')
+@pytest.mark.skip
+@pytest.mark.parametrize('schema_filename, expected', [
+    ('yandextank/plugins/Telegraf/config/schema.yaml',
+     """kill_old
+========
+:default: True
+:type: list
+:elements:
+ :type: string
+ :allowed: foo
+
+default_target
+==============
+:default: localhost
+:any of:
+ - :type: list
+   :elements:
+    :type: string
+ - :type: string  
+   :allowed: auto  """)
 ])
-def test_preserve_indents(s, expected):
-    assert RSTFormatter.preserve_indents(TextBlock(s)) == expected
+def test_format_schema(schema_filename, expected):
+    raise NotImplemented
