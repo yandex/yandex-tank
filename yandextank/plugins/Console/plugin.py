2e625638d5f0f3d010f6d94c52ad0fef0d19a8c2
Mikhail Dyomin <f2nd@yandex-team.ru>
2017-11-26 16:59:51 +0300

    Console impovements:
        - Cumulative percentiles
        - Lots of sparklines (aka inline charts)
        - Extended cases stats
        - Fixed size info
        - Better handling small-size terminals
        - More flexible layout configuration

--- yandextank/plugins/Console/plugin.py
+++ yandextank/plugins/Console/plugin.py
@@ -7,8 +7,6 @@ import traceback
 from .screen import Screen
 from ...common.interfaces import AbstractPlugin, AggregateResultListener
 
-LOG = logging.getLogger(__name__)
-
 
 class Plugin(AbstractPlugin, AggregateResultListener):
     ''' Console plugin '''
@@ -16,15 +14,21 @@ class Plugin(AbstractPlugin, AggregateResultListener):
 
     def __init__(self, core, cfg, cfg_updater):
         AbstractPlugin.__init__(self, core, cfg, cfg_updater)
+        self.log = logging.getLogger(__name__)
         self.screen = None
         self.render_exception = None
         self.console_markup = None
-        self.info_panel_width = '33'
-        self.short_only = 0
+        self.info_panel_width = self.get_option("info_panel_width")
+        self.short_only = self.get_option("short_only")
         # these three provide non-blocking console output
         self.__console_view = None
         self.__writer_thread = None
         self.__writer_event = None
+        self.cases_sort_by = self.get_option("cases_sort_by")
+        self.cases_max_spark = self.get_option("cases_max_spark")
+        self.max_case_len = self.get_option("max_case_len")
+        self.times_max_spark = self.get_option("times_max_spark")
+        self.sizes_max_spark = self.get_option("sizes_max_spark")
 
     @staticmethod
     def get_key():
@@ -37,22 +41,25 @@ class Plugin(AbstractPlugin, AggregateResultListener):
         ]
 
     def configure(self):
-        self.info_panel_width = self.get_option(
-            "info_panel_width", self.info_panel_width)
-        self.short_only = self.get_option("short_only", '0')
-        if not self.get_option("disable_all_colors", '0'):
+        if not self.get_option("disable_all_colors"):
             self.console_markup = RealConsoleMarkup()
         else:
             self.console_markup = NoConsoleMarkup()
-        for color in self.get_option("disable_colors", '').split(' '):
+        for color in self.get_option("disable_colors").split(' '):
             self.console_markup.__dict__[color] = ''
-        self.screen = Screen(self.info_panel_width, self.console_markup)
-
+        self.screen = Screen(
+            self.info_panel_width, self.console_markup,
+            cases_sort_by=self.cases_sort_by,
+            cases_max_spark=self.cases_max_spark,
+            max_case_len=self.max_case_len,
+            times_max_spark=self.times_max_spark,
+            sizes_max_spark=self.sizes_max_spark
+        )
         try:
             aggregator = self.core.job.aggregator
             aggregator.add_result_listener(self)
         except KeyError:
-            LOG.debug("No aggregator for console")
+            self.log.debug("No aggregator for console")
             self.screen.block_rows = []
             self.screen.info_panel_percent = 100
 
@@ -63,7 +70,7 @@ class Plugin(AbstractPlugin, AggregateResultListener):
 
             if self.__console_view:
                 if not self.short_only:
-                    LOG.debug("Writing console view to STDOUT")
+                    self.log.debug("Writing console view to STDOUT")
                     sys.stdout.write(self.console_markup.clear)
                     sys.stdout.write(self.__console_view)
                     sys.stdout.write(self.console_markup.TOTAL_RESET)
@@ -79,7 +86,7 @@ class Plugin(AbstractPlugin, AggregateResultListener):
         try:
             self.__console_view = self.screen.render_screen().encode('utf-8')
         except Exception as ex:
-            LOG.warn("Exception inside render: %s", traceback.format_exc(ex))
+            self.log.warn("Exception inside render: %s", traceback.format_exc(ex))
             self.render_exception = ex
             self.__console_view = ""
 
@@ -93,8 +100,8 @@ class Plugin(AbstractPlugin, AggregateResultListener):
 
             quantiles = dict(
                 zip(
-                    overall['interval_real']['q']['q'], overall['interval_real']
-                    ['q']['value']))
+                    overall['interval_real']['q']['q'],
+                    overall['interval_real']['q']['value']))
             info = (
                 "ts:{ts}\tRPS:{rps}\tavg:{avg_rt:.2f}\t"
                 "min:{min:.2f}\tmax:{q100:.2f}\tq95:{q95:.2f}\t").format(
@@ -105,14 +112,14 @@ class Plugin(AbstractPlugin, AggregateResultListener):
                     min=overall['interval_real']['min'] / 1000.0,
                     q100=quantiles[100] / 1000,
                     q95=quantiles[95] / 1000)
-            LOG.info(info)
+            self.log.info(info)
         else:
             self.screen.add_second_data(data)
 
     def add_info_widget(self, widget):
         ''' add right panel widget '''
         if not self.screen:
-            LOG.debug("No screen instance to add widget")
+            self.log.debug("No screen instance to add widget")
         else:
             self.screen.add_info_widget(widget)
 
@@ -132,7 +139,7 @@ class RealConsoleMarkup(object):
     YELLOW = '\033[1;33m'
     RED = '\033[1;31m'
     RED_DARK = '\033[31;3m'
-    RESET = '\033[1;m' + WHITE_ON_BLACK  # fix for MacOS console
+    RESET = WHITE_ON_BLACK + '\033[1;m'
     CYAN = "\033[1;36m"
     GREEN = "\033[1;32m"
     WHITE = "\033[1;37m"
@@ -142,13 +149,19 @@ class RealConsoleMarkup(object):
     BG_BROWN = '\033[1;43m'
     BG_CYAN = '\033[1;46m'
 
+    def get_markup_vars(self):
+        result = []
+        for val in [
+            self.YELLOW, self.RED, self.RESET, self.CYAN, self.BG_MAGENTA,
+            self.WHITE, self.BG_GREEN, self.GREEN, self.BG_BROWN,
+            self.RED_DARK, self.MAGENTA, self.BG_CYAN
+        ]:
+            result.append(val)
+        return result
+
     def clean_markup(self, orig_str):
         ''' clean markup from string '''
-        for val in [
-                self.YELLOW, self.RED, self.RESET, self.CYAN, self.BG_MAGENTA,
-                self.WHITE, self.BG_GREEN, self.GREEN, self.BG_BROWN,
-                self.RED_DARK, self.MAGENTA, self.BG_CYAN
-        ]:
+        for val in self.get_markup_vars():
             orig_str = orig_str.replace(val, '')
         return orig_str
 
