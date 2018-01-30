ba8fe93098c511bd6b8ae2548440d437f69d30b9
netort <netort@yandex-team.ru>
2018-01-30 20:09:00 +0300

    client patches, logging -> logger

--- yandextank/plugins/Telegraf/config.py
+++ yandextank/plugins/Telegraf/config.py
@@ -145,7 +145,7 @@ class ConfigManager(object):
             elif (str(metric.tag)).lower() == 'telegrafraw':
                 telegrafraw.append(metric.text)
         if len(host_config) == 0:
-            logging.info('Empty host config, using defaults')
+            logger.info('Empty host config, using defaults')
             for section in defaults_enabled:
                 host_config[section] = defaults[section]
         result = {
