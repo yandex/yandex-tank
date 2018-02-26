28300af03d77b8525296d33d7f77949e6e13df19
fomars <fomars@yandex-team.ru>
2018-02-26 19:46:43 +0300

    extra requirement

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ analytic tools for the results they produce.
         'pandas>=0.18.0', 'numpy>=1.12.1', 'future>=0.16.0',
         'pip>=8.1.2',
         'matplotlib>=1.5.3', 'seaborn>=0.7.1',
-        'pyyaml>=3.12', 'cerberus>=1.1', 'influxdb>=5.0.0', 'mock',
+        'pyyaml>=3.12', 'cerberus>=1.1', 'influxdb>=5.0.0',
     ],
     setup_requires=[
         'pytest-runner', 'flake8',
