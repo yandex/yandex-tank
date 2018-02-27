0fa9e1ca06d2a6ae7d55fc5c78fea18e185657cd
fomars <fomars@yandex-team.ru>
2018-02-27 22:46:21 +0300

    send yaml to lunapark

--- yandextank/core/tankcore.py
+++ yandextank/core/tankcore.py
@@ -133,12 +133,7 @@ class TankCore(object):
     @property
     def cfg_snapshot(self):
         if not self._cfg_snapshot:
-            if self.cfg_depr:
-                output = StringIO()
-                self.cfg_depr.write(output)
-                self._cfg_snapshot = output.getvalue()
-            else:
-                self._cfg_snapshot = str(self.config)
+            self._cfg_snapshot = str(self.config)
         return self._cfg_snapshot
 
     @staticmethod
