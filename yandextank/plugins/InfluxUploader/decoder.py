5ff33778e930e45ca996f1f63386896fd9015e8c
Torubarov Timur <ttorubarov@ozon.ru>
2019-03-11 17:18:53 +0300

    influx plugin rewritten, added histograms, appropriate per-label stats

--- yandextank/plugins/InfluxUploader/decoder.py
+++ yandextank/plugins/InfluxUploader/decoder.py
@@ -6,168 +6,186 @@ def uts(dt):
 
 
 class Decoder(object):
-    def __init__(self, tank_tag, uuid):
-        self.tank_tag = tank_tag
-        self.uuid = uuid
+    """
+    Decode metrics incoming from tank into points for InfluxDB client
 
-    def decode_monitoring_item(self, item):
-        host, metrics, _, ts = item
-        return {
-            "measurement": "monitoring",
-            "tags": {
-                "tank": self.tank_tag,
-                "host": host,
-                "uuid": self.uuid,
-            },
-            "time": ts,
-            "fields": metrics,
+    Parameters
+    ----------
+    parent_tags : dict
+        common per-test tags
+    tank_tag : basestring
+        tank identifier tag
+    uuid : basestring
+        test id tag
+    labeled : bool
+        detailed stats for each label
+    histograms : bool
+        response time histograms measurements
+    """
+    def __init__(self, tank_tag, uuid, parent_tags, labeled, histograms):
+        self.labeled = labeled
+        initial_tags = {
+            "tank": tank_tag,
+            "uuid": uuid
         }
+        initial_tags.update(parent_tags)
+        self.tags = initial_tags
+        self.histograms = histograms
+
+    def set_uuid(self, id_):
+        self.tags['uuid'] = id_
 
     def decode_monitoring(self, data):
-        points = []
+        """
+        The reason why we have two separate methods for monitoring
+        and aggregates is a strong difference in incoming data.
+        """
+        points = list()
         for second_data in data:
-            timestamp = second_data["timestamp"]
             for host, host_data in second_data["data"].iteritems():
-                points += [{
-                    "measurement": "monitoring",
-                    "tags": {
-                        "tank": self.tank_tag,
-                        "uuid": self.uuid,
-                        "host": host,
-                        "comment": host_data["comment"],
-                    },
-                    "time": timestamp,
-                    "fields": {  # quantiles
-                        metric: value
-                        for metric, value in host_data["metrics"].iteritems()
-                    },
-                }]
+                points.append(
+                    self.__make_points(
+                        "monitoring",
+                        {"host": host, "comment": host_data.get("comment")},
+                        second_data["timestamp"],
+                        {
+                            metric: value
+                            for metric, value in host_data["metrics"].iteritems()
+                        }
+                    )
+                )
         return points
 
-    def decode_aggregate_labeled(self, data, stat, prefix):
-        timestamp = int(data["ts"])
-        points = []
-        # overall quantiles w/ __OVERALL__ label
-        points += [
-            {
-                "measurement": prefix + "overall_quantiles",
-                "tags": {
-                    "tank": self.tank_tag,
-                    "uuid": self.uuid,
-                    "label": "__OVERALL__"
-                },
-                "time": timestamp,
-                "fields": {
-                    'q' + str(q): value / 1000.0
-                    for q, value in zip(data["overall"]["interval_real"]["q"]["q"],
-                                        data["overall"]["interval_real"]["q"]["value"])
-                },
-            }
-        ]
-        for key, value in data["tagged"].items():
-            points += [{
-                "measurement": prefix + "overall_quantiles",
-                "tags": {
-                    "tank": self.tank_tag,
-                    "uuid": self.uuid,
-                    "label": key
-                },
-                "time": timestamp,
-                "fields": {  # quantiles
-                    'q' + str(q): value / 1000.0
-                    for q, value in zip(value["interval_real"]["q"]["q"],
-                                        value["interval_real"]["q"]["value"])
-                },
-            }, {
-                "measurement": prefix + "overall_meta",
-                "tags": {
-                    "tank": self.tank_tag,
-                    "uuid": self.uuid,
-                    "label": key,
-                },
-                "time": timestamp,
-                "fields": {
-                    "active_threads": stat["metrics"]["instances"],
-                    "RPS": value["interval_real"]["len"],
-                    "planned_requests": float(stat["metrics"]["reqps"]),
-                },
-            }, {
-                "measurement": prefix + "net_codes",
-                "tags": {
-                    "tank": self.tank_tag,
-                    "uuid": self.uuid,
-                    "label": key
-                },
-                "time": timestamp,
-                "fields": {
-                    str(code): int(cnt)
-                    for code, cnt in value["net_code"]["count"].items()
-                },
-            }, {
-                "measurement": prefix + "proto_codes",
-                "tags": {
-                    "tank": self.tank_tag,
-                    "uuid": self.uuid,
-                    "label": key
-                },
-                "time": timestamp,
-                "fields": {
-                    str(code): int(cnt)
-                    for code, cnt in value["proto_code"]["count"].items()
-                },
-            },
-            ]
+    def decode_aggregates(self, aggregated_data, gun_stats, prefix):
+        ts = aggregated_data["ts"]
+        points = list()
+        # stats overall w/ __OVERALL__ label
+        points += self.__make_points_for_label(
+            ts,
+            aggregated_data["overall"],
+            "__OVERALL__",
+            prefix,
+            gun_stats
+        )
+        # detailed stats per tag
+        if self.labeled:
+            for label, aggregated_data_by_tag in aggregated_data["tagged"].items():
+                points += self.__make_points_for_label(
+                    ts,
+                    aggregated_data_by_tag,
+                    label,
+                    prefix,
+                    gun_stats
+                )
         return points
 
-    def decode_aggregate(self, data, stat):
-        timestamp = int(data["ts"])
-        points = [
-            {
-                "measurement": "overall_quantiles",
-                "tags": {
-                    "tank": self.tank_tag,
-                    "uuid": self.uuid,
-                },
-                "time": timestamp,
-                "fields": {  # quantiles
-                    'q' + str(q): value / 1000.0
-                    for q, value in zip(data["overall"]["interval_real"]["q"]["q"],
-                                        data["overall"]["interval_real"]["q"]["value"])
-                },
-            }, {
-                "measurement": "overall_meta",
-                "tags": {
-                    "tank": self.tank_tag,
-                    "uuid": self.uuid,
-                },
-                "time": timestamp,
-                "fields": {
-                    "active_threads": stat["metrics"]["instances"],
-                    "RPS": data["overall"]["interval_real"]["len"],
-                    "planned_requests": float(stat["metrics"]["reqps"]),
-                },
-            }, {
-                "measurement": "net_codes",
-                "tags": {
-                    "tank": self.tank_tag,
-                    "uuid": self.uuid,
-                },
-                "time": timestamp,
-                "fields": {
-                    str(code): int(cnt)
-                    for code, cnt in data["overall"]["net_code"]["count"].items()
-                },
-            }, {
-                "measurement": "proto_codes",
-                "tags": {
-                    "tank": self.tank_tag,
-                    "uuid": self.uuid,
-                },
-                "time": timestamp,
-                "fields": {
-                    str(code): int(cnt)
-                    for code, cnt in data["overall"]["proto_code"]["count"].items()
-                },
-            },
-        ]
-        return points
+    def __make_points_for_label(self, ts, data, label, prefix, gun_stats):
+        """x
+        Make a set of points for `this` label
+
+        overall_quantiles, overall_meta, net_codes, proto_codes, histograms
+        """
+        label_points = list()
+
+        label_points.extend(
+            (
+                # overall quantiles for label
+                self.__make_points(
+                    prefix + "overall_quantiles",
+                    {"label": label},
+                    ts,
+                    self.__make_quantile_fields(data)
+                ),
+                # overall meta (gun status) for label
+                self.__make_points(
+                    prefix + "overall_meta",
+                    {"label": label},
+                    ts,
+                    self.__make_overall_meta_fields(data, gun_stats)
+                ),
+                # net codes for label
+                self.__make_points(
+                    prefix + "net_codes",
+                    {"label": label},
+                    ts,
+                    self.__make_netcodes_fields(data)
+                ),
+                # proto codes for label
+                self.__make_points(
+                    prefix + "proto_codes",
+                    {"label": label},
+                    ts,
+                    self.__make_protocodes_fields(data)
+                )
+            )
+        )
+        # histograms, one row for each bin
+        if self.histograms:
+            for bin_, count in zip(data["interval_real"]["hist"]["bins"],
+                                   data["interval_real"]["hist"]["data"]):
+                label_points.append(
+                    self.__make_points(
+                        prefix + "histograms",
+                        {"label": label},
+                        ts,
+                        {"bin": bin_, "count": count}
+                    )
+                )
+        return label_points
+
+    @staticmethod
+    def __make_quantile_fields(data):
+        return {
+            'q' + str(q): value / 1000.0
+            for q, value in zip(data["interval_real"]["q"]["q"],
+                                data["interval_real"]["q"]["value"])
+        }
+
+    @staticmethod
+    def __make_overall_meta_fields(data, stats):
+        return {
+            "active_threads": stats["metrics"]["instances"],
+            "RPS": data["interval_real"]["len"],
+            "planned_requests": float(stats["metrics"]["reqps"]),
+        }
+
+    @staticmethod
+    def __make_netcodes_fields(data):
+        return {
+            str(code): int(cnt)
+            for code, cnt in data["net_code"]["count"].items()
+        }
+
+    @staticmethod
+    def __make_protocodes_fields(data):
+        return {
+            str(code): int(cnt)
+            for code, cnt in data["proto_code"]["count"].items()
+        }
+
+    def __make_points(self, measurement, additional_tags, ts, fields):
+        """
+        Parameters
+        ----------
+        measurement : string
+            measurement type (e.g. monitoring, overall_meta, net_codes, proto_codes, overall_quantiles)
+        additional_tags : dict
+            custom additional tags for this points
+        ts : integer
+            timestamp
+        fields : dict
+            influxdb columns
+
+        Returns
+        -------
+        dict
+            points for InfluxDB client
+        """
+        tags = self.tags.copy()
+        tags.update(additional_tags)
+        return {
+            "measurement": measurement,
+            "tags": tags,
+            "time": int(ts),
+            "fields": fields,
+        }
--- yandextank/plugins/InfluxUploader/plugin.py
+++ yandextank/plugins/InfluxUploader/plugin.py
@@ -35,30 +35,32 @@ class Plugin(AbstractPlugin, AggregateResultListener,
     def __init__(self, core, cfg, name):
         AbstractPlugin.__init__(self, core, cfg, name)
         self.tank_tag = self.get_option("tank_tag")
-        address = self.get_option("address")
-        port = self.get_option("port")
-        self.client = InfluxDBClient(
-            address,
-            port,
-            username=self.get_option("username"),
-            password=self.get_option("password"),
-            database=self.get_option("database"),
-        )
-        self.labeled = self.get_option("labeled")
         self.prefix_measurement = self.get_option("prefix_measurement")
-        self.custom_tags = self.get_option("custom_tags")
-        grafana_root = self.get_option("grafana_root")
-        grafana_dashboard = self.get_option("grafana_dashboard")
-        uuid = str(uuid4())
-        logger.info(
-            "Grafana link: {grafana_root}"
-            "dashboard/db/{grafana_dashboard}?var-uuid={uuid}&from=-5m&to=now".format(
-                grafana_root=grafana_root,
-                grafana_dashboard=grafana_dashboard,
-                uuid=uuid,
-            )
+        self._client = None
+        self.start_time = None
+        self.end_time = None
+        self.decoder = Decoder(
+            self.tank_tag,
+            str(uuid4()),
+            self.get_option("custom_tags"),
+            self.get_option("labeled"),
+            self.get_option("histograms"),
         )
-        self.decoder = Decoder(self.tank_tag, uuid)
+
+    @property
+    def client(self):
+        if not self._client:
+            self._client = InfluxDBClient(
+                self.get_option("address"),
+                self.get_option("port"),
+                username=self.get_option("username"),
+                password=self.get_option("password"),
+                database=self.get_option("database"),
+            )
+        return self._client
+
+    def prepare_test(self):
+        self.core.job.subscribe_plugin(self)
 
     def start_test(self):
         self.start_time = datetime.datetime.now()
@@ -67,33 +69,24 @@ class Plugin(AbstractPlugin, AggregateResultListener,
         self.end_time = datetime.datetime.now() + datetime.timedelta(minutes=1)
         return retcode
 
-    def prepare_test(self):
-        self.core.job.subscribe_plugin(self)
-
     def on_aggregated_data(self, data, stats):
-        if self.client:
-            if self.labeled:
-                points = self.decoder.decode_aggregate_labeled(data, stats, self.prefix_measurement)
-            else:
-                points = self.decoder.decode_aggregate(data, stats)
-            if len(self.custom_tags):
-                self.add_custom_tags(points)
-            self.client.write_points(points, 's')
-
-    def add_custom_tags(self, points):
-        for p in points:
-            common_tags = p['tags']
-            common_tags.update(self.custom_tags)
-            p['tags'] = common_tags
+        self.client.write_points(
+            self.decoder.decode_aggregates(data, stats, self.prefix_measurement),
+            's'
+        )
 
     def monitoring_data(self, data_list):
-        if self.client:
-            if len(data_list) > 0:
-                [
-                    self._send_monitoring(chunk)
-                    for chunk in chop(data_list, self.get_option("chunk_size"))
-                ]
+        if len(data_list) > 0:
+            [
+                self._send_monitoring(chunk)
+                for chunk in chop(data_list, self.get_option("chunk_size"))
+            ]
 
     def _send_monitoring(self, data):
-        points = self.decoder.decode_monitoring(data)
-        self.client.write_points(points, 's')
+        self.client.write_points(
+            self.decoder.decode_monitoring(data),
+            's'
+        )
+
+    def set_uuid(self, id_):
+        self.decoder.tags['uuid'] = id_
