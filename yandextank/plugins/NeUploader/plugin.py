b6ae11c5e10078a2cf2f068bc6a61b48292cef53
Nadya Mirgorodskaya <szypulka@yandex-team.ru>
2019-04-24 14:06:40 +0300

    Add cases support to Neuploader plugin

--- yandextank/plugins/NeUploader/plugin.py
+++ yandextank/plugins/NeUploader/plugin.py
@@ -1,3 +1,4 @@
+""" Plugin uploading metrics from yandextank to Luna. """
 import logging
 from netort.data_manager import DataSession
 
@@ -11,12 +12,16 @@ logger = logging.getLogger(__name__)  # pylint: disable=C0103
 class Plugin(AbstractPlugin, MonitoringDataListener):
     SECTION = 'neuploader'
 
+    columns = ['interval_real', 'connect_time', 'send_time',
+               'latency', 'receive_time', 'interval_event']
+
     def __init__(self, core, cfg, name):
         super(Plugin, self).__init__(core, cfg, name)
         self._is_telegraf = None
         self.clients_cfg = [{'type': 'luna',
                              'api_address': self.cfg.get('api_address'),
                              'db_name': self.cfg.get('db_name')}]
+        self.metrics_ids = {}  # map of case names and metric local ids
 
     def configure(self):
         pass
@@ -32,10 +37,7 @@ class Plugin(AbstractPlugin, MonitoringDataListener):
             self.data_session = DataSession({'clients': self.clients_cfg})
             self.add_cleanup(self.cleanup)
             self.data_session.update_job({'name': self.cfg.get('test_name')})
-            col_map_aggr = {name: 'metric %s' % name for name in
-                            ['interval_real', 'connect_time', 'send_time', 'latency',
-                             'receive_time', 'interval_event']}
-            self.uploader = get_uploader(self.data_session, col_map_aggr, True)
+            self.uploader = self.get_uploader()
 
     def cleanup(self):
         uploader_metainfo = self.map_uploader_tags(self.core.status.get('uploader'))
@@ -43,9 +45,9 @@ class Plugin(AbstractPlugin, MonitoringDataListener):
         self.data_session.close()
 
     def is_test_finished(self):
-        df = next(self.reader)
-        if df is not None:
-            self.uploader(df)
+        datafr = next(self.reader)
+        if datafr is not None:
+            self.uploader(datafr)
         return -1
 
     def monitoring_data(self, data_list):
@@ -55,11 +57,68 @@ class Plugin(AbstractPlugin, MonitoringDataListener):
         for chunk in self.reader:
             if chunk is not None:
                 self.uploader(chunk)
+        # TODO: create cases
         return retcode
 
     @property
     def is_telegraf(self):
         return True
+
+    def metric_generator(self, col, case):
+        """
+        Generator of metric objects:
+        Checks existent metrics and creates new aggregated metric if it does not exist.
+        :param col:  str with column name
+        :param case: str with case name
+        :return: metric object
+        """
+
+        metric_obj = self.metrics_ids[col].get(case)
+        if not metric_obj:
+            parent = self.metrics_ids[col].get('overall')
+            metric_obj = self.data_session.new_aggregated_metric('metric {} {}'.format(col, case), parent=parent)
+            self.metrics_ids[col][case] = metric_obj.local_id
+        return metric_obj
+
+    def get_uploader(self):
+        """
+        Creates metric_obj on every metric for every case in DataFrame and puts all them to queue.
+        :return: upload_df function
+        """
+
+        self.metrics_ids = {column: {} for column in self.columns}
+
+        def upload_df(datafr):
+            """
+            Every metric in dataframe should be aggregated twice if it has not null tag column
+            :param datafr: input chunk with DataFrame
+            :return: function
+            """
+            df_cases_set = set([row.tag for row in datafr.itertuples() if row.tag])
+
+            for column in self.columns:
+                overall_metric_obj = self.metric_generator(column, 'overall')
+                overall_metric_obj.put(datafr)
+
+                for case_name in df_cases_set:
+                    case_metric_obj = self.metric_generator(column, case_name)
+                    self.metrics_ids[column][case_name] = case_metric_obj.local_id
+
+                    result_df = self.filter_df_by_case(datafr, case_name)
+                    result_df['value'] = result_df[column]
+                    case_metric_obj.put(result_df)
+
+        return upload_df
+
+    @staticmethod
+    def filter_df_by_case(datafr, case):
+        """
+        Filter dataframe by case name. If case is 'overall', return the whole dataframe.
+        :param datafr: DataFrame
+        :param case: str with case name
+        :return: DataFrame
+        """
+        return datafr if case == 'overall' else datafr.loc[datafr['tag'] == case]
 
     @staticmethod
     def map_uploader_tags(uploader_tags):
@@ -76,18 +135,3 @@ class Plugin(AbstractPlugin, MonitoringDataListener):
                 (k, v) for k, v in uploader_tags.get('meta', {}).items()
             ]
         )
-
-
-def get_uploader(data_session, column_mapping, overall_only=False):
-    """
-    :type column_mapping: dict
-    :type data_session: DataSession
-    """
-    overall = {col_name: data_session.new_aggregated_metric(name + ' overall')
-               for col_name, name in column_mapping.items()}
-
-    def upload_df(df):
-        for col_name, metric in overall.items():
-            df['value'] = df[col_name]
-            metric.put(df)
-    return upload_df
