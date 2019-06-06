25b41af6beaedcce6dbc72ae4c22f44125d29377
fomars <fomars@yandex-team.ru>
2019-06-06 16:30:16 +0300

    netort -0.5.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ analytic tools for the results they produce.
         'psutil>=1.2.1', 'requests>=2.5.1', 'paramiko>=1.16.0',
         'pandas>=0.18.0', 'numpy>=1.12.1', 'future>=0.16.0',
         'pip>=8.1.2',
-        'pyyaml>=4.2b1', 'cerberus==1.2', 'influxdb>=5.0.0', 'netort>=0.4.2',
+        'pyyaml>=4.2b1', 'cerberus==1.2', 'influxdb>=5.0.0', 'netort>=0.5.0',
         'retrying==1.3.3', 'pytest-benchmark==3.2.2'
     ],
     setup_requires=[
