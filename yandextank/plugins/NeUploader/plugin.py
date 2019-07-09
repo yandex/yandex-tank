d58ebcab5023c74924b8c7da614c207f5c14e1a1
Nadya Mirgorodskaya <szypulka@yandex-team.ru>
2019-07-09 11:56:30 +0300

    Fix case when empty values stored in meta

--- yandextank/plugins/NeUploader/plugin.py
+++ yandextank/plugins/NeUploader/plugin.py
@@ -152,21 +152,11 @@ class Plugin(AbstractPlugin, MonitoringDataListener):
         return df if case == '__overall__' else df.loc[df['tag'] == case]
 
     def map_uploader_tags(self, uploader_tags):
-        try:
-            result_tags = dict(
-                [
-                    ('component', uploader_tags.get('component')),
-                    ('description', uploader_tags.get('job_dsc')),
-                    ('name', uploader_tags.get('job_name', self.cfg.get('test_name'))),
-                    ('person', uploader_tags.get('person')),
-                    ('task', uploader_tags.get('task')),
-                    ('version', uploader_tags.get('version')),
-                    ('lunapark_jobno', uploader_tags.get('job_no'))
-                ] + [
-                    (k, v) for k, v in uploader_tags.get('meta', {}).items()
-                ]
-            )
-        except AttributeError:
+        if not uploader_tags:
             logger.info('No uploader metainfo found')
-            result_tags = {}
-        return result_tags
+            return {}
+        else:
+            meta_tags_names = ['component', 'description', 'name', 'person', 'task', 'version', 'lunapark_jobno']
+            meta_tags = {key: uploader_tags.get(key, self.cfg.get(key)) for key in meta_tags_names}
+            meta_tags.update({k: v for k, v in uploader_tags.get('meta', {}).items()})
+            return meta_tags
