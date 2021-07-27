221cb4ecf4a3be312b9e135eb78163385ac4bbd2
ival83 <ival83@yandex-team.ru>
2021-07-27 19:47:42 +0300

    fix-conflicts
    ref:1d6a1f6d456b1130dfe550113d1bab42727939ae

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ It uses other load generators such as JMeter, ab or phantom inside of it for
 load generation and provides a common configuration system for them and
 analytic tools for the results they produce.
 ''',
-    python_requires='==3.*',
+    python_requires='==3.7.*, 3.8.*',
     maintainer='Yandex Load Team',
     maintainer_email='load@yandex-team.ru',
     url='http://yandex.github.io/yandex-tank/',
