9a68b507749cb3af504bce09b9d79a75a2388675
nsamokhin <nsamokhin@yandex-team.com>
2024-06-27 10:02:20 +0300

    yandextank yc monitoring plugin fix compatibility with YC agent
    fix compatibility with python 3.11
    62d6f3607745455db8bb5eca7d19550c94adf236

--- yandextank/plugins/YCMonitoring/sensor.py
+++ yandextank/plugins/YCMonitoring/sensor.py
@@ -1,6 +1,6 @@
 import re
 import json
-from datetime import datetime, UTC
+from datetime import datetime, timezone
 from time import time
 from queue import Queue
 
@@ -44,8 +44,8 @@ class YCMonitoringSensor(MonitoringSensorProtocol):
         # returns valid data: query='"objects_count"{service="storage"}'
         self.data = {
             'query': query,
-            'fromTime': datetime.fromtimestamp(time(), tz=UTC).isoformat(),
-            'toTime': (datetime.fromtimestamp(time() + 1, tz=UTC)).isoformat(),
+            'fromTime': datetime.fromtimestamp(time(), tz=timezone.utc).isoformat(),
+            'toTime': (datetime.fromtimestamp(time() + 1, tz=timezone.utc)).isoformat(),
             'downsampling': {'gridInterval': 1500},
         }
         self.queue = queue
@@ -64,7 +64,7 @@ class YCMonitoringSensor(MonitoringSensorProtocol):
         if self.is_got_some_data_after_parsing:
             self.data['fromTime'] = self.data['toTime']
             self.is_got_some_data_after_parsing = False
-        self.data['toTime'] = datetime.fromtimestamp(time() + 1, tz=UTC).isoformat()
+        self.data['toTime'] = datetime.fromtimestamp(time() + 1, tz=timezone.utc).isoformat()
         self.prepare_data(self.get_data())
 
     @observetime('YCMonitoringSensor.prepare_data', LOGGER)
